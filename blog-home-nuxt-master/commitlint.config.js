001c6521b34ee0b6f28854dfb5b59461d895cfd7
zhu wei <724607039@qq.com>
2024-09-23 21:56:34 +0800

    'feat:测试校验工具'

--- blog-home-nuxt-master/commitlint.config.js
+++ blog-home-nuxt-master/commitlint.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'config',
+        'chore',
+        'bulid',
+        'ci',
+        'Chores'
+      ]
+    ],
+    'subject-full-stop': [0, 'never'],
+    'subject-case:': [0, 'never'],
+  },
 }
